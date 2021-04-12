d1b01950f52b38fe7ad15e50ade5a85127bd5047
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-04-12 09:23:09 +0100

    🛴 Defining transaction route.

--- src/app.ts
+++ src/app.ts
@@ -8,24 +8,6 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("The API is up and running");
 });
 
-// add transaction route
-app.get(
-  "/add-transaction",
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log("Adding a transaction...");
-    res.send("Transaction has been added!");
-  }
-);
-
-// delete transaction route
-app.get(
-  "/delete-transaction",
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log("Deleting a transaction...");
-    res.send("Transaction has been deleted!");
-  }
-);
-
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
 );
---
+++ src/routes/transaction.ts
@@ -0,0 +1,21 @@
+import { IRouter, Router, Request, Response } from "express";
+
+const router: IRouter = Router();
+
+// add transaction route
+router.get("/add-transaction", (req: Request, res: Response) => {
+  console.log("Adding a transaction...");
+  console.log("Transaction has been added!");
+});
+
+// delete transaction route
+router.get("/delete-transaction", (req: Request, res: Response) => {
+  console.log("Deleting a transaction...");
+  console.log("Transaction has been deleted!");
+});
+
+// refund transaction route
+router.get("/refund-transaction", (req: Request, res: Response) => {
+  console.log("Refunding a transaction...");
+  console.log("Transaction has been refunded!");
+});
