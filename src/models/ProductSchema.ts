525a2864aea7b9db92c4416a68b0b8971c6c796b
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-04-12 10:10:59 +0100

    🐱‍👤 Defining ProductModel.

---
+++ src/models/ProductSchema.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { ProductTypes } from "./Product";
+
+// defining product schema
+const productSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  productType: {
+    type: ProductTypes,
+    required: true,
+  },
+});
+
+export const Product = mongoose.model("Product", productSchema);
